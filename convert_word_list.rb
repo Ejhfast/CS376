9e764280b0265227a8ae238db5996ecdd4cf2fac
Ethan Fast <ethan@Ethans-MacBook-Air-2.local>
2013-05-26 15:45:22 -0700

    latest

--- convert_word_list.rb
+++ convert_word_list.rb
@@ -4,8 +4,11 @@ g1 = ["a","b","c","d","e","f","g"]
 g2 = ["h","i","j","k","l","m","n"]
 g3 = ["o","p","q","r","s","t","u"]
 g4 = ["v","w","x","y","z"]
+avg = 0.0
+count = 0
 word_list = IO.read("en_50k.txt").split("\n").map{|line| line.split(" ")}.each do |pair|
-  
+  avg += pair[1].to_i
+  count += 1
   gp = pair[0].split(//).map do |x|
     if g1.include?(x)
       "1"
@@ -22,6 +25,7 @@ word_list = IO.read("en_50k.txt").split("\n").map{|line| line.split(" ")}.each d
   key = gp.join("")
   hsh[key] = hsh[key] + [[pair[0],pair[1].to_i]]
 end
+#puts avg / count
 puts "window.Dictionary=#{hsh.to_json}"
 # hsh.each do |k,v|
 #   if v.size > 1
--- public/app.js
+++ public/app.js
@@ -1,13 +1,36 @@
 $(document).ready(function(){
-  function getResults(arr){
-    var query = _.map(arr, function(x){
-      if(x === "A1") return "1";
-      else if(x === "A2") return "2";
-      else if(x === "A4") return "3";
-      else if(x === "A5") return "4";
-    }).join("");
-    return _.sortBy(Dictionary[query], function(x){return -1*x[1]});
-  };
+  $('#submit_word').click(function(){
+    var g1 = ["a","b","c","d","e","f","g"],
+        g2 = ["h","i","j","k","l","m","n"],
+        g3 = ["o","p","q","r","s","t","u"],
+        g4 = ["v","w","x","y","z"],
+        word = $('#new_word').val(),
+        mapping = _.filter(_.map(word.split(''), function(letter){
+          if(g1.indexOf(letter) > -1){
+            return "1";
+          }
+          else if(g2.indexOf(letter) > -1){
+            return "2";
+          }
+          else if(g3.indexOf(letter) > -1){
+            return "3";
+          }
+          else if(g4.indexOf(letter) > -1){
+            return "4";
+          }
+          else{
+            return null;
+          }
+        }), function(x){return x !== null}).join("");
+        console.log(word);
+        console.log(mapping);
+        if(Dictionary[mapping] && Dictionary[mapping].length > 0){
+          Dictionary[mapping] = Dictionary[mapping] + [[word, (_.reduce(Dictionary[mapping], function(x,y){return x+y},0) / Dictionary[mapping].length)]]
+        }
+        else{
+          Dictionary[mapping] = [[word, 2398]]; // average for file
+        }
+  });
   window.stack = [];
   setInterval(function(){
     $.get("/finger_data", function(resp){
@@ -17,10 +40,31 @@ $(document).ready(function(){
               // If the current selection (which was bent) is now straight, then allow for it to be selected again
               current_selection = null;
       }
-      var selected = _.sortBy(_.filter(parse_response, function(x){return x[1] == "bent!"}), 
-        function(x){return 1*x[2]})[0];
+      var total_selected = _.sortBy(_.filter(parse_response, function(x){return x[1] == "bent!"}), 
+        function(x){return 1*x[2]}),
+        selected = total_selected[0];
+      if(total_selected.length === 0){
+        window.called_delete = false;
+      }
       if(selected && selected.length > 0){
-        if(selected[0] === "A0"){
+        if(total_selected.length === 5){
+          console.log(total_selected);
+          if(window.stack.length > 0){
+            window.stack = [];
+            current_selection = null;
+            window.last_enter = null;
+            $('#letters').html("");
+            $('#word_list').html("");
+          }
+          else{
+            if(window.called_delete === false){
+              $('#text').children().last().remove();
+              console.log("removing");
+              window.called_delete = true;
+            }
+          }
+        }
+        else if(selected[0] === "A0"){
           if(window.last_enter !== null){
             console.log(window.last_enter.results);
             var index = window.last_enter.index,
@@ -76,18 +120,27 @@ $(document).ready(function(){
           }).join("");
           var results = _.sortBy(Dictionary[query], function(x){return -1*x[1]});
           $('#word_list').html("");
-          _.each(_.first(results,10), function(x,i){
-            var extra = "";
-            if(i === 0){
-              extra = "<div id='idx'>></div>";
-            }
-            $('#word_list').append("<div class='word'>"+x[0]+extra+"</div>");
-          });
-          $('#letters').append("<div class='letter "+selected[0]+"'>"+selected[0]+"</div>");
-          $(".sec."+selected[0]).fadeIn(100).fadeOut(100).fadeIn(100);
-          console.log(selected[0]);
-          window.current_selection = selected[0];
-          window.last_enter = null;
+          //if(results.length > 0){
+            _.each(_.first(results,10), function(x,i){
+              var extra = "";
+              if(i === 0){
+                extra = "<div id='idx'>></div>";
+              }
+              $('#word_list').append("<div class='word'>"+x[0]+extra+"</div>");
+            });
+            $('#letters').append("<div class='letter "+selected[0]+"'>"+selected[0]+"</div>");
+            $(".sec."+selected[0]).fadeIn(100).fadeOut(100).fadeIn(100);
+            console.log(selected[0]);
+            window.current_selection = selected[0];
+            window.last_enter = null;
+          //}
+          // else{
+          //   window.stack = [];
+          //   current_selection = null;
+          //   window.last_enter = null;
+          //   $('#letters').html("");
+          //   $('#word_list').html("");
+          // }
         }
       }
     });
